10461d502648b59d9c55d336166f3a310e8d102d
gerardsmakowski-digital <gerardsmakowski@gmail.com>
2026-01-28 16:59:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -220,7 +220,8 @@ with col_right:
     height=450, 
     margin=dict(t=80, b=50, l=10, r=10),
     paper_bgcolor='rgba(0,0,0,0)',
-    plot_bgcolor='rgba(0,0,0,0)'
+    plot_bgcolor='rgba(0,0,0,0)',
+        text-align: center
 )
     
     st.plotly_chart(fig_right, use_container_width=True)
